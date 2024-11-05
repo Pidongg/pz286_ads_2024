4fc66200eaee60d90f992545b72e3618451ce511
Pidongg <graciepy1@gmail.com>
2024-11-05 11:39:44 +0000

    practical 1: add function housing_upload_join_data(...) to access

--- fynesse/access.py
+++ fynesse/access.py
@@ -1,6 +1,7 @@
 from .config import *
 import requests
 import pymysql
+import csv
 
 """These are the types of import we might expect in this file
 import httplib2
@@ -20,19 +21,22 @@ def data():
 
 
 def download_price_paid_data(year_from, year_to):
-    # Base URL where the dataset is stored 
+    # Base URL where the dataset is stored
     base_url = "http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com"
     """Download UK house price data for given year range"""
     # File name with placeholders
     file_name = "/pp-<year>-part<part>.csv"
     for year in range(year_from, (year_to+1)):
-        print (f"Downloading data for year: {year}")
-        for part in range(1,3):
-            url = base_url + file_name.replace("<year>", str(year)).replace("<part>", str(part))
+        print(f"Downloading data for year: {year}")
+        for part in range(1, 3):
+            url = base_url + \
+                file_name.replace("<year>", str(year)).replace(
+                    "<part>", str(part))
             response = requests.get(url)
             if response.status_code == 200:
                 with open("." + file_name.replace("<year>", str(year)).replace("<part>", str(part)), "wb") as file:
                     file.write(response.content)
+
 
 def create_connection(user, password, host, database, port=3306):
     """ Create a database connection to the MariaDB database
@@ -57,3 +61,26 @@ def create_connection(user, password, host, database, port=3306):
     except Exception as e:
         print(f"Error connecting to the MariaDB Server: {e}")
     return conn
+
+
+def housing_upload_join_data(conn, year):
+    start_date = str(year) + "-01-01"
+    end_date = str(year) + "-12-31"
+
+    cur = conn.cursor()
+    print('Selecting data for year: ' + str(year))
+    cur.execute(f'SELECT pp.price, pp.date_of_transfer, po.postcode, pp.property_type, pp.new_build_flag, pp.tenure_type, pp.locality, pp.town_city, pp.district, pp.county, po.country, po.latitude, po.longitude FROM (SELECT price, date_of_transfer, postcode, property_type, new_build_flag, tenure_type, locality, town_city, district, county FROM pp_data WHERE date_of_transfer BETWEEN "' +
+                start_date + '" AND "' + end_date + '") AS pp INNER JOIN postcode_data AS po ON pp.postcode = po.postcode')
+    rows = cur.fetchall()
+
+    csv_file_path = 'output_file.csv'
+
+    # Write the rows to the CSV file
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        # Write the data rows
+        csv_writer.writerows(rows)
+    print('Storing data for year: ' + str(year))
+    cur.execute(f"LOAD DATA LOCAL INFILE '" + csv_file_path +
+                "' INTO TABLE `prices_coordinates_data` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"' LINES STARTING BY '' TERMINATED BY '\n';")
+    print('Data stored for year: ' + str(year))
