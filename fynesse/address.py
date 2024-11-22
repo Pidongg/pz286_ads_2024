bce5f1de91f68133303e2dadc6e266ac2ee1d27a
Pidongg <graciepy1@gmail.com>
2024-11-22 23:26:04 +0000

    move functions from assess to address

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,5 +1,8 @@
 # This file contains code for suporting addressing questions in the data
 import osmnx as ox
+from sklearn.manifold import MDS
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def count_pois_near_coordinates(latitude: float, longitude: float, tags: dict, distance_km: float = 1.0) -> dict:
@@ -23,3 +26,48 @@ def count_pois_near_coordinates(latitude: float, longitude: float, tags: dict, d
         pois_counts[tag] = pois[tag].notnull().sum()
 
     return pois_counts
+
+
+def plot_osm_data(data_frames, labels, north, south, east, west, colours=None):
+    fig, ax = plt.subplots()
+    graph = ox.graph_from_bbox(north, south, east, west)
+
+    # Retrieve nodes and edges
+    nodes, edges = ox.graph_to_gdfs(graph)
+    # Plot street edges
+    edges.plot(ax=ax, linewidth=1, edgecolor="dimgray")
+
+    ax.set_xlim([west, east])
+    ax.set_ylim([south, north])
+    ax.set_xlabel("longitude")
+    ax.set_ylabel("latitude")
+    if colours is None:
+        colours = ["blue"]*len(data_frames)
+    # Plot tourist places
+    for i, data_frame in enumerate(data_frames):
+        data_frame.plot(
+            ax=ax, color=colours[i], alpha=1, markersize=50, label=labels[i])
+    plt.legend()
+    plt.tight_layout()
+
+
+def mds_visualisation(distance_matrix, title):
+    mds = MDS(n_components=2, dissimilarity='precomputed')  # 2D visualization
+    pos = mds.fit_transform(distance_matrix)
+    fig, ax = plt.subplots(figsize=(10, 8))  # Adjust figure size as needed
+
+    ax.scatter(pos[:, 0], pos[:, 1])  # Plot the points
+    for i, label in enumerate(distance_matrix.index.to_list()):
+        ax.annotate(label, (pos[i, 0], pos[i, 1]),
+                    xytext=(5, 5),  # Offset the text slightly
+                    textcoords='offset points',
+                    ha='center', va='bottom')
+    plt.title(title)
+    plt.show()
+
+
+def heatmap_plot(data_frame, title):
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(data_frame, annot=True, cmap='coolwarm', linewidths=0.5)
+    plt.title(title)
+    plt.show()
